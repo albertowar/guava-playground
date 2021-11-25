48cadcc1ad80d4c43f8d23b5cc14375a2fc3f54f
Alberto Guerra Gonzalez <aguerra@riotgames.com>
2021-11-25 20:03:53 +0000

    Initialize

---
+++ src/main/java/org/example/CacheReader.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import com.google.common.cache.LoadingCache;
+import java.util.concurrent.ExecutionException;
+
+public class CacheReader implements Runnable {
+  private LoadingCache<String, String> cache;
+
+  public CacheReader(LoadingCache<String, String> cache) {
+    this.cache = cache;
+  }
+
+  @Override
+  public void run() {
+    try {
+      System.out.printf("Thread %s. Reading value at %s\n", Thread.currentThread().getName(), System.currentTimeMillis());
+      System.out.printf("Thread %s. Value %s at %s\n", Thread.currentThread().getName(), this.cache.get("something"), System.currentTimeMillis());
+    } catch (ExecutionException e) {
+      System.out.printf("Thread %s. Exception %s\n", Thread.currentThread().getName(), e);
+    }
+  }
+}
